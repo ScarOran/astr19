9aad265071e8493dd66db866f88b3578ff50581e
ScarOran <95949988+ScarOran@users.noreply.github.com>
2022-04-17 18:57:56 -0700

    Add files via upload
    Code Journal #1 Session Prompt #2

---
+++ prompt-two.py
@@ -0,0 +1,27 @@
+#write a python code that prints the sum of two floating
+#point numbers, the difference between two integers
+#and the product of a floating point number and an integer.
+#In each case, have the program print out the data type
+#of the resulting answer
+
+#defining the floating numbers 
+float_one = 12.0
+float_two = 5.0
+
+#defining the integers
+int_one = 12
+int_two = 5
+
+#defining float and integer
+float_mult = 7.0
+int_mult = 8
+
+
+#the sum of two floats
+print(float_one, '+', float_two, '=', float_one + float_two)
+
+#the difference of two integers
+print(int_one, '-', int_two, '=', int_one - int_two)
+
+#the product of a float and integer
+print(float_mult, '*', int_mult, '=', float_mult * int_mult)
