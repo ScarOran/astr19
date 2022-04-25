3d96e9660820138859a2598aebffe7cf8e8fbe6a
ScarOran <95949988+ScarOran@users.noreply.github.com>
2022-04-24 22:09:18 -0700

    Add files via upload
    Live Demonstration Code Journal Unit 1

---
+++ live-code-one.py
@@ -0,0 +1,14 @@
+import sys
+import numpy
+
+#create the main function
+def main():
+	x = 0.0
+	for i in range(0,19):
+		x = i * 2 + 19
+		print(f'The value of x is {x}')
+
+#create conditional
+if __name__=='__main__':
+	main()
+
